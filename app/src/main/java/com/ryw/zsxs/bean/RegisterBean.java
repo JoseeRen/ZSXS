3ea54d77f0e70fcc30fb28d24492e823ccddb8a2
Zm <2390235300@qq.com>
2017-07-02 15:54:36 +0800

    注册页面有点小问题

---
+++ app/src/main/java/com/ryw/zsxs/bean/RegisterBean.java
@@ -0,0 +1,19 @@
+package com.ryw.zsxs.bean;
+
+/**
+ * Created by Zhao on 2017/7/2.
+ */
+
+public class RegisterBean {
+
+    /**
+     * errorMessage : 该手机号已被注册
+     * errorCode : -3
+     */
+
+    public String errorMessage;
+    public int errorCode;
+    public String Mycode;
+
+
+}
---
+++ app/src/main/java/com/ryw/zsxs/bean/YanZhengMaBean.java
@@ -0,0 +1,16 @@
+package com.ryw.zsxs.bean;
+
+/**
+ * Created by Zhao on 2017/7/2.
+ */
+
+public class YanZhengMaBean {
+
+    /**
+     * codes :
+     */
+
+    public String codes;
+
+
+}
